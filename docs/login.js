5417fabfab3a2dc9270da1fcef30e65ba0e569fc
yulim <yulim13sky@naver.com>
2023-05-27 21:44:19 +0900

    modify login page

--- docs/login.js
+++ docs/login.js
@@ -1,6 +1,3 @@
-$("#login-button").click(function(event){
-    event.preventDefault();
-  
-  $('form').fadeOut(500);
-  $('.wrapper').addClass('form-success');
+$("#login-button").click(function (event) {
+
 });
